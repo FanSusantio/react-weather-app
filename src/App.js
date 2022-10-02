068caf64d603e91f09bf22da45e96a86a438ff66
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-10-03 10:30:01 +1100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/fan-susantio-62227919/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Fan Susantio{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/FanSusantio/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
