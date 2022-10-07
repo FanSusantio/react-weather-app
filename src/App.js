6e8b0bed3f91ca857986faf458f1f148198168f1
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-10-08 00:03:25 +1100

    import boostrap

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/fan-susantio-62227919/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Fan Susantio{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/FanSusantio/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/fan-susantio-62227919/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fan Susantio{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/FanSusantio/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
