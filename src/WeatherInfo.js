873f3a866d99a7d12c4285fcb5a7658f48bc3ab1
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-10-11 00:12:58 +1100

    incorporate weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,7 +16,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-start">
-              <img src={props.data.iconUrl} alt={props.data.description} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="float-start">
               <div className="WeatherTemperature">
