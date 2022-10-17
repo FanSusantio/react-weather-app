9c5e4fca5c6b58f692bb05cd931947b8bd5476b0
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-10-18 00:27:46 +1100

    created API connection and 1 Day Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,37 @@
-import React from "react";
-export default function WeatherForecast() {
-  return <div className="WeatherForecast">Weather Forecast Section</div>;
+import React, { useState } from "react";
+
+import WeatherForecastDay from "./WeatherForecastDay";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "300c5b69978f9b056c3bb931ff9c185d";
+    let latitude = props.coord.lat;
+    let longitude = props.coord.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
